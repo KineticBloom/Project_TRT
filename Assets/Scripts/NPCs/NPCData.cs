16275d2d9c55d730bc22ec915cab94a7da661fbb
Shinoyhe <ehyoshin@ucsc.edu>
2025-04-16 12:48:37 -0700

    Playtest 4/16/25

--- Assets/Scripts/NPCs/NPCData.cs
+++ Assets/Scripts/NPCs/NPCData.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 using System.Linq;
+#if UNITY_EDITOR
 using MackySoft.SerializeReferenceExtensions;   // Don't Remove, this is actually needed
+#endif
 
 [CreateAssetMenu(fileName = "New NPCData", menuName = "ScriptableObjects/NPCData"), System.Serializable]
 public class NPCData : ScriptableObject
@@ -16,6 +18,8 @@ public class NPCData : ScriptableObject
     [TextArea] public string Bio;
 
     [Header("Bartering Details")]
+    [Tooltip("The Flag ID of the character's barter")]
+    public string FlagID;
     [Tooltip("Defines the Effect Cards that will affect the bartering game")]
     [SerializeReference, SubclassSelector]
     public List<EffectCard> EffectCards = new List<EffectCard>();

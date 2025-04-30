844bd03340625ea72e91d4479172d9583f7af572
Shinoyhe <ehyoshin@ucsc.edu>
2025-04-30 06:51:23 -0700

    Build 4/30/25 Fixes

--- Assets/Scripts/Camera/WorldCameraController.cs
+++ Assets/Scripts/Camera/WorldCameraController.cs
@@ -90,9 +90,9 @@ public class WorldCameraController : MonoBehaviour
     [Label("Auto-Update Cameras")] [Tooltip("Automatically updates the child virtual cameras when changing the script's parameters." +
         "Disable if you want to manually change the virtual cameras.")]
     [SerializeField] private bool autoUpdate = true;
+#endif
     [BoxGroup("Controls"), Tooltip("Enable if you wish to manually set the camera's targets")]
     [SerializeField] private bool manualTargets = false;
-#endif
     #endregion
 
     #region ======== [ PRIVATE PROPERTIES ] ========

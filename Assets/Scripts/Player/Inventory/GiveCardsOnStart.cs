a47c1aa47f6ac8424fb89acc9da7ca3fc17382bf
tblagarrett <tblagarrett@gmail.com>
2025-05-06 11:10:00 -0700

    Give Cards On Start

---
+++ Assets/Scripts/Player/Inventory/GiveCardsOnStart.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiveCardsOnStart : MonoBehaviour
+{
+    [SerializeField] private List<InventoryCardData> cards;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GameManager.Instance == null) { return; }
+        
+        foreach (InventoryCardData card in cards)
+        {
+            GameManager.Inventory.AddCard(card);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
